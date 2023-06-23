a87147c8087da443be2705b191b967c0c9f8d8e0
AContiSG <84248592+AContiSG@users.noreply.github.com>
2023-06-23 13:40:47 -0300

    cart

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-    User.findById("64947bae64a5b64275571a1b")
+    User.findById("6495c158d44badf695a17b22")
         .then((user) => {
-            req.user = user;
+            req.user = new User(user.name, user.email, user.cart, user._id);
             next();
         })
         .catch((err) => console.log(err));
