ff79192496298bcc8f81d4fa8547c080816cf8b0
AContiSG <84248592+AContiSG@users.noreply.github.com>
2023-06-30 13:14:08 -0300

    finished session and cookies module

--- app.js
+++ app.js
@@ -35,7 +35,10 @@ app.use(
 );
 
 app.use((req, res, next) => {
-    User.findById("64976d102979dacadd2d58ee")
+    if (!req.session.user) {
+        return next();
+    }
+    User.findById(req.session.user._id)
         .then((user) => {
             req.user = user;
             next();
